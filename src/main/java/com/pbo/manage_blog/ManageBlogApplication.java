4be5f0a6ec5a1af985b0a403c712d1bb6cc9fb13
Kingkin Fajar <kingkinfajar8@gmail.com>
2025-04-11 09:44:08 +0700

    chore: init setup project

---
+++ src/main/java/com/pbo/manage_blog/ManageBlogApplication.java
@@ -0,0 +1,13 @@
+package com.pbo.manage_blog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ManageBlogApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ManageBlogApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/pbo/manage_blog/ManageBlogApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pbo.manage_blog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ManageBlogApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
